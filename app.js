d368fec3e916a2163c258b203d1a2fd431091251
CrisSenac <crislene.ssilva1@senacsp.edu.br>
2024-06-12 17:02:06 -0300

    aula 12-06

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 import express from 'express'
+import routerLivros from './routes/livrosRouter.js'
 
 const app = express()
+app.use(express.json())
 
 const port = 8000
 
-app.get('/', (req, res) => {
-    res.send("Cris Sansi 2")
-})
+// Rota GET para a raiz a rota principal
+app.use('/livros', routerLivros);
 
+// Iniciar o servidor
 app.listen(port, () =>{
     console.log(`Estudando na porta ${port}`)
 })
---
+++ routes/livrosRouter.js
@@ -0,0 +1,24 @@
+import { getLivro } from '../controller/livroController.js';
+import {Router} from 'express'
+
+const router = Router();
+
+// GET - Retornar livros
+router.get('/', getLivro)
+
+// POST - Criar um livro
+router.post('/', (req, res) => {
+    res.send('Estou na rota Post')
+})
+
+// PATCH - Atualizar o livro
+router.patch('/', (req, res) => {
+    res.send('Estou na rota Patch')
+})
+
+// DELETE - Apagar um livro
+router.delete('/', (req, res) => {
+    res.send('Estou na rota Delete')
+})
+
+export default router;
